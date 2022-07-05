94d095da720349985b9c0a55ac73356c9b421911
mehdi "Arcanite" Oueslati <mehdi.oueslati@epita.fr>
2022-07-06 01:13:10 +0200

    now can generate p vectors

--- main.cpp
+++ main.cpp
@@ -42,27 +42,25 @@ int main(int argc, char const *argv[])
 	if(parse_items(argv[1], features, items)){ return 1; }
 	if(parse_sessions(argv[2], sessions)){ return 1; }
 
-	for(auto i = sessions->begin(); i != sessions->end(); ++i){
-		std::cout << i->first << std::endl;
-		for (auto j = i->second.item_ids.begin(); j != i->second.item_ids.end(); ++j)
-		{
-			std::cout << '\t' << *j << std::endl;
-		}
-	}
-
 	auto item_28143 = items->find(28143);
 	auto q_28143 = item_28143->second.convert_to_vector(features);
-	auto q_vec_handler = VectorHandler<EPSILON>("qs/q_");
+
+/*	auto q_vec_handler = VectorHandler<EPSILON>("qs/q_");
 	q_vec_handler.save(0, q_28143);
 	std::map<int, float> q_out;
 	q_vec_handler.load(0, &q_out);
+*/
+	auto session_19 = sessions->find(19);
+	auto p_19 = session_19->second.convert_to_vector(items, features);
+	auto p_vec_handler = VectorHandler<EPSILON>("ps/p_");
+	p_vec_handler.save(0, p_19);
 
-	std::cout << mul_vec(&q_out, &q_out) << std::endl;
-
-
+	//std::cout << mul_vec(&q_out, &q_out) << std::endl;
 
     //cleanup
+    delete p_19;
     delete q_28143;
+    delete sessions;
 	delete features;
 	delete items;
 
--- session.hpp
+++ session.hpp
@@ -9,6 +9,38 @@ struct Session {
 	std::vector<int> item_ids;
 
 	Session() : id(0){}
+	std::map<int, float> *convert_to_vector(
+		std::unordered_map<int, Item> *items,
+		std::map<std::string, Feature> *feature_map
+	){
+
+		auto out = new std::map<int, float>();
+
+		for (auto item_id = this->item_ids.begin(); item_id != this->item_ids.end(); ++item_id)
+		{
+			auto current_item = items->find(*item_id);
+			auto current_item_as_vector = current_item->second.convert_to_vector(feature_map);
+
+			for (
+				auto feature = current_item_as_vector->begin();
+				feature != current_item_as_vector->end();
+				++feature
+			){
+				const auto feature_id = feature->first;
+				const auto feature_value = feature->second;
+
+				if(out->contains(feature_id)){
+					out->find(feature_id)->second += feature_value;
+				} else {
+					out->emplace(std::make_pair(feature_id, feature_value));
+				}
+			}
+
+			delete current_item_as_vector;
+		}
+
+		return out;
+	}
 };
 
 #endif //SESSION
